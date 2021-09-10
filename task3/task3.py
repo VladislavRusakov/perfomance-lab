8cdbd245068362df83a74d5d285a748ceb872f26
Vlad <ruzikvlad5@yandex.ru>
2021-09-10 22:13:29 +0300

    task3

---
+++ task3/task3.py
@@ -0,0 +1,35 @@
+from sys import argv
+import json
+
+
+tests = argv[1]
+values = argv[2]
+
+
+with open(tests, 'r') as f:
+    tests_json = json.load(f)
+
+
+with open(values, 'r') as f:
+    values_json = json.load(f)
+
+
+def recursive_search(tests: dict) -> None:
+    """"Рекурсивная функция поиска id в tests.json и добавления им value из values.json"""
+    if type(tests) == dict:
+        for instance in values_json['values']:
+            if instance['id'] == tests['id']:
+                tests['value'] = instance['value']
+        if 'values' in tests:
+            recursive_search(tests['values'])
+
+    elif type(tests) == list:
+        for instance in tests:
+            recursive_search(instance)
+        
+recursive_search(tests_json['tests'])
+
+with open('report.json', 'w') as f:
+    json.dump(tests_json, f)
+
+
